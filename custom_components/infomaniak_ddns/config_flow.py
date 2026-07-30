f5d7f6d4944d085315a7cd95b11d402a66108c83
cy-bertrand <116965831+cy-bertrand@users.noreply.github.com>
2026-07-30 15:47:17 +0200

    indentation bug

--- custom_components/infomaniak_ddns/config_flow.py
+++ custom_components/infomaniak_ddns/config_flow.py
@@ -282,8 +282,10 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
 class OptionsFlowHandler(config_entries.OptionsFlow):
 
-    async def async_step_init(self, user_input=None) -> FlowResult:
-    errors: dict[str, str] = {}
+    async def async_step_init(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        errors: dict[str, str] = {}
 
         if user_input is not None:
             if not user_input.get(CONF_PASSWORD):
